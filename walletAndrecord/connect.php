2ff4228544fe98ed96155202e05a29153f50d99f
karaage0o <gina98239@gmail.com>
2025-05-15 18:14:32 +0800

    改connect.php

--- walletAndrecord/connect.php
+++ walletAndrecord/connect.php
@@ -1,7 +1,7 @@
 <?php
 // db.php
-// $host = "localhost"; // 資料庫主機
-$host = "127.0.0.1";
+$host = "localhost"; // 資料庫主機
+// $host = "127.0.0.1";
 $username = "root";  // 資料庫使用者
 $password = "";      // 資料庫密碼
 $dbname = "junglebite"; // 資料庫名稱
