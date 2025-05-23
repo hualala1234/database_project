365cc869afa0a3a3b8255e8b2b4185868ae985e4
nohairpig <b124020033@g-mail.nsysu.edu.tw>
2025-05-23 16:03:41 +0800

    登入後的下拉式選單檢查完成

--- claw_machine/claw.php
+++ claw_machine/claw.php
@@ -89,7 +89,9 @@ $cid = $_SESSION['cid'] ?? '';
                                         <a href="../walletAndrecord/c_record.php?cid=<?php echo $cid; ?>&role=c" class="dropdown-item text-decoration-none">交易紀錄</a>
                                         <a href="../customer/friends.php?cid=<?php echo $cid; ?>&role=c" class="dropdown-item text-decoration-none">我的好友</a>
                                         <a href="../wheel/wheel.php?cid=<?php echo $cid; ?>&role=c" class="dropdown-item text-decoration-none">命運轉盤</a>
+                                        <a href="../customer/myfavorite.php?cid=<?php echo $cid; ?>&role=c" class="dropdown-item text-decoration-none">我的愛店</a>
                                         <a href="/database_project/customer/reservation.php" class="dropdown-item text-decoration-none">我要訂位</a>
+ 
                                     <?php elseif ($_SESSION['role'] === 'd'): ?>
                                         <a href="/database/customer/setting.php" class="dropdown-item">外送員設定</a>
                                     <?php elseif ($_SESSION['role'] === 'platform'): ?>
